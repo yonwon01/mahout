75233b062ca8608548fd9379cbf2f78800071045
seowon <yonwon01@naver.com>
2017-11-27 16:09:17 +0900

    recommender algo

---
+++ src/main/java/com/bigdata2017/mahout/RecommenderIntro.java
@@ -0,0 +1,40 @@
+package com.bigdata2017.mahout;
+
+
+import org.apache.mahout.cf.taste.impl.model.file.*;
+import org.apache.mahout.cf.taste.impl.neighborhood.*;
+import org.apache.mahout.cf.taste.impl.recommender.*;
+import org.apache.mahout.cf.taste.impl.similarity.*;
+import org.apache.mahout.cf.taste.model.*;
+import org.apache.mahout.cf.taste.neighborhood.*;
+import org.apache.mahout.cf.taste.recommender.*;
+import org.apache.mahout.cf.taste.similarity.*;
+import java.io.*;
+import java.util.*;
+
+class RecommenderIntro {
+
+  private RecommenderIntro() {
+  }
+
+  public static void main(String[] args) throws Exception {
+
+    DataModel model = new FileDataModel(new File("C:/Users/bit-user/Desktop/source/intro.csv"));
+
+    UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+    UserNeighborhood neighborhood =
+      new NearestNUserNeighborhood(2, similarity, model);
+
+    Recommender recommender = new GenericUserBasedRecommender(
+        model, neighborhood, similarity);
+
+    List<RecommendedItem> recommendations =
+        recommender.recommend(1, 2);
+
+    for (RecommendedItem recommendation : recommendations) {
+      System.out.println(recommendation);
+    }
+
+  }
+
+}
